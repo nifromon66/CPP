a0b9e82ed4fd9b972c60a30a2334968292568125
nifromon <nifromon@student.42perpignan.fr>
2025-09-21 13:37:01 +0200

    continued

--- Module_09/ex00/src/main.cpp
+++ Module_09/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nifromon <nifromon@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 16:52:45 by nifromon          #+#    #+#             */
-/*   Updated: 2025/09/19 18:06:53 by nifromon         ###   ########.fr       */
+/*   Updated: 2025/09/21 13:28:16 by nifromon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,6 @@
 #include <string>
 #include <iostream>
 
-std::string parse_line(std::string line) {
-	std::string	result;
-	char 		delimiter = '|';
-	size_t		pos = line.find(delimiter);
-
-	if (pos != std::string::npos) {
-		
-	}
-}
 
 int main(int argc, char *argv[]) {
 	
@@ -37,7 +28,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	std::string data_line;
-	char delimiter = ',';
+	char 		delimiter = ',';
 	while (std::getline(data_file, data_line)) {
 		
 		size_t pos = data_line.find(delimiter);
@@ -49,6 +40,8 @@ int main(int argc, char *argv[]) {
 	
 	data_file.close();
 
+	std::map<std::string, double> input_map;
+	
 	std::ifstream input_file(argv[1]);
 	if (!input_file.is_open()) {
 		std::cerr << RED "Cannot open file : " << argv[1] << "." RESET << std::endl;
@@ -56,6 +49,29 @@ int main(int argc, char *argv[]) {
 	}
 	
 	std::string input_line;
+	char		delimiter = '|';
+	while (std::getline(input_file, input_line)) {
+
+		size_t	pos = input_line.find(delimiter);
+
+		if (pos != std::string::npos) {
+			input_map.insert({input_line.substr(0, pos - 1), std::stod(input_line.substr(pos + 2))});
+		}
+		else {
+			std::cerr << RED "Error: Incorrect input syntax." RESET << std::endl;
+			return (1);
+		}
+	}
+
+	input_file.close();
+
+	std::map<std::string, double> result_map;
+	std::map<std::string, double>::iterator it_data = data_map.begin();
+	std::map<std::string, double>::iterator it_input = input_map.begin();
+
+	
+	
+	
 	
 	
 }
